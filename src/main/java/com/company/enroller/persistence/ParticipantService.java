552a79ca772ec75ebd576475428fa6aa29407f9b
Gosia Data <data@student.agh.edu.pl>
2023-04-02 12:20:22 +0200

    update

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +23,36 @@ public class ParticipantService {
 		return query.list();
 	}
 
+    public Participant findByLogin(String login) {
+		String hql = "FROM Participant WHERE login =: login";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", login);
+		return (Participant) connector.getSession().get(Participant.class, login);
+
+		//List<Participant> results = query.getResultList();
+		//if(!result.isEmpty()){
+		//return results.get(0);
+		//} else {
+		//return null;
+		//}
+    }
+
+	public Participant add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		return participant;
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+		}
+
+	public void update(Participant foundParticipant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(foundParticipant);
+		transaction.commit();
+	}
 }
